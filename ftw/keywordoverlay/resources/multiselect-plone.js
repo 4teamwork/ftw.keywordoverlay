aa5ba3dcaa609b3f3c6a07eaf8142314a91cdb55
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2010-09-02 14:35:52 +0000

    added allowedRole support for keywordoverlay

--- ftw/keywordoverlay/resources/multiselect-plone.js
+++ ftw/keywordoverlay/resources/multiselect-plone.js
@@ -1,40 +1,42 @@
 //plone integration of ui.multiselectwidget
 jq(function(){
     //get all keywordwidgets
-    var $targets = jq('.field.ArchetypesKeywordWidget select[id$=existing_keywords]');
+    var $targets = jq('.field.ArchetypesKeywordWidget select');
     //bind click event on every fieldset that contains an keywordwidget
     $targets.each(function(i,o){
         $target = jq(o);
         //get id and cut off <fieldset->
         var loadonID = $target.closest('fieldset').attr('id').substr(9);
+        var add_id = $target[0].id.replace("_existing","");
         $loadon = jq('ul.formTabs li.formTab [href$='+loadonID+']');
         //load widget while clicking on formTab
         $loadon.bind('click', function(e){
             $target.css('width','500px');
-            
+
             //load widget
             $target.multiselect({sortable: false});
         });
 
         //hide plones default add textarea
         var $addarea = $target.parent().prev();
-        $addarea.hide();
-        
-        //area for new items
-        var content  = '<div><ul class="new_item_space"></ul>';
-        content += '<form>Add new item <input class="item" name="new_item" type="text" /><input class="allowMultiSubmit" type="submit" value="+"></form></div>';
         var $selected = $target.closest('div');
-        $selected.after(content);
+        if (jq('#'+add_id).parent().length > 0) {
+            $addarea.hide();
+            //area for new items
+            var content  = '<div><ul class="new_item_space"></ul>';
+            content += '<form>Add new item <input class="item" name="new_item" type="text" /><input class="allowMultiSubmit" type="submit" value="+"></form></div>';
+            $selected.after(content);
+        }
         //add eventhandler to add and delete items
         $field = $selected.parent();
         $field.find('.allowMultiSubmit').bind('click', function(e){
             e.preventDefault();
             var item = jq(this).prev();
-            var itemval = item.val()
+            var itemval = item.val();
             item.val('');
             if (itemval){
                 var delbutton = '<a class="delbutton" href="#"><img src="'+portal_url+'/delete_icon.gif" /></a>';
-                var toadd = '<li class="ui-state-default ui-element"><span>'+itemval+'</span> '+delbutton+'</li>'
+                var toadd = '<li class="ui-state-default ui-element"><span>'+itemval+'</span> '+delbutton+'</li>';
                 //add to the visible part
                 jq(this).parent().prev().append(toadd);
                 //add item to the hidden area (for plone)
@@ -45,7 +47,7 @@ jq(function(){
         //delete function
         jq('.delbutton').live('click',function(e){
             e.preventDefault();
-            var $this = jq(this)
+            var $this = jq(this);
             var $parent = $this.parent();
             var itemval = $this.prev().html();
             $addarea = jq($this.closest('.ArchetypesKeywordWidget').find('[id$=_keywords]').eq(0));
