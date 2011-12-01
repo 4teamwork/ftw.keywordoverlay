acb491be8f0b77383af953002e0e09d4b0ce3c26
Jonas Baumann <jone@jone.ch>
2011-12-01 16:14:21 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -6,29 +6,44 @@ maintainer = 'Mathias Leimgruber'
 
 setup(name='ftw.keywordoverlay',
       version=version,
-      description="Puts a nice overlay over Archetypes' default keywordwidget for plone.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description="Puts a nice overlay over Archetypes' default " + \
+          "keywordwidget for plone.",
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='plone widget archetypes keyword jquery',
-      author='Mathias Leimgruber',
-      author_email='m.leimgruber@4teamwork.ch',
-      url='http://www.4teamwork.ch',
-      license='GPL',
+
+      keywords='plone widget archetypes keyword jquery ftw keywordoverlay',
+      author='4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.keywordoverlay',
+      license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-          'setuptools',
-          'collective.js.jqueryui',
-          'collective.js.ui.multiselect',
-      ],
-      entry_points="""
+        'setuptools',
+        'collective.js.jqueryui',
+        'collective.js.ui.multiselect',
+        ],
+
+      entry_points='''
       # -*- Entry points: -*-
-      """,
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
       )
